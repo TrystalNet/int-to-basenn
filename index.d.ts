2fffff237de61cc843398df2972eaf4615ae51d3
Terry O'Neill <terry.oneill@trystal.net>
2017-02-01 21:35:20 -0700

    updates the d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,33 @@
-declare const BASE62CHARS : string
-declare const BASE64CHARS : string
-declare const BASE26CHARS : string
-declare const BASE27CHARS : string
+export as namespace IntToBaseNN;
+export = IntToBaseNN;
 
-interface IEncodeFunc { (intval:number) : string }
-interface IDecodeFunc { (strval:string) : number }
+declare namespace IntToBaseNN {
+  const BASE62CHARS : string
+  const BASE64CHARS : string
+  const BASE26CHARS : string
+  const BASE27CHARS : string
 
-declare class Converter {
-  constructor(charset:string)
-  encode : IEncodeFunc
-  decode : IDecodeFunc
+  interface IEncodeFunc { (intval:number) : string }
+  interface IDecodeFunc { (strval:string) : number }
+
+  class Converter {
+    constructor(charset:string)
+    encode : IEncodeFunc
+    decode : IDecodeFunc
+  }
+
+  function encode(intval:number, charset:string, validate:boolean):string;
+  function decode(strval:string, charset:string, validate:boolean):number;
+
+  const Base62 : Converter
+  const base62Encode : IEncodeFunc
+  const base62Decode : IDecodeFunc
+
+  const Base64 : Converter
+  const base64Encode : IEncodeFunc
+  const base64Decode : IDecodeFunc
+
+  const Base26 : Converter
+  const base26Encode : IEncodeFunc
+  const base26Decode : IDecodeFunc
 }
-
-declare function encode(intval:number, charset:string, validate:boolean):string;
-declare function decode(strval:string, charset:string, validate:boolean):number;
-
-declare const Base62 : Converter
-declare const base62Encode : IEncodeFunc
-declare const base62Decode : IDecodeFunc
-
-declare const Base64 : Converter
-declare const base64Encode : IEncodeFunc
-declare const base64Decode : IDecodeFunc
-
-declare const Base26 : Converter
-declare const base26Encode : IEncodeFunc
-declare const base26Decode : IDecodeFunc
